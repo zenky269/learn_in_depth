1c207434bd68cc52d61cab8fa8fee4325289f09a
Ahmed Mohammed <ahmed107hasn@gmail.com>
2023-12-29 04:12:37 -0800

    first c on repo

---
+++ unit2/lesson3/test_printf.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : test_printf.c
+ Author      : Ahmed Mohammed
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("hello\n");
+	return EXIT_SUCCESS;
+}
